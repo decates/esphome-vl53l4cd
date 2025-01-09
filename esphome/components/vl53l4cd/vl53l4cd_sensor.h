67cec88e11c44ba52174c59d9bf34abeac25b553
Mark de Cates <mdecates@defaqto.com>
2025-01-09 14:27:06 +0000

    Another attempt to fix include issue

--- esphome/components/vl53l4cd/vl53l4cd_sensor.h
+++ esphome/components/vl53l4cd/vl53l4cd_sensor.h
@@ -6,7 +6,7 @@
 #include "esphome/core/hal.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/i2c/i2c.h"
-#include "./stm32duino_VL53L4CD/vl53l4cd_class.h"
+#include "vl53l4cd_class.h"
 
 namespace esphome {
 namespace vl53l4cd {
